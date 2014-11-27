f616f862c5bb08915e906819790d3d9ea7931235
spwebguy <spwebguy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-27 08:55:25 +0000

    Version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/user-access-shortcodes/tags/1.1@1033718 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uasc.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Plugin Name: User Access Shortcodes
+ * Plugin URI: http://wpdarko.com/darko-tools/user-access-shortcodes/
+ * Description: "The most simple way of controlling who sees what on your website". This plugin adds a button to your post editor, allowing you to restrict content to logged in users only (or guests) with a simple shortcode. Find support and information on the <a href="http://wpdarko.com/user-access-shortcodes/">plugin's page</a>. This is a free plugin, it is NOT limited and does not contain any ad.
+ * Version: 1.1
+ * Author: WP Darko
+ * Author URI: http://wpdarko.com
+ * License: GPL2
+ */
+
+function uasc_free_pro_check() {
+    if (is_plugin_active('user-access-shortcodes-pro/uasc_pro.php')) {
+        
+        function my_admin_notice(){
+        echo '<div class="updated">
+                <p><strong>PRO</strong> version is activated.</p>
+              </div>';
+        }
+        add_action('admin_notices', 'my_admin_notice');
+        
+        deactivate_plugins(__FILE__);
+    }
+}
+
+add_action( 'admin_init', 'uasc_free_pro_check' );
+
+add_action( 'admin_head', 'uasc_css' );
+
+function uasc_css()
+{
+    echo '
+    <style>
+        i.mce-i-uasc-mce-icon {
+	       background-image: url("http://localhost:8888/plugin_dev/wp-content/plugins/user-access-shortcodes/img/uasc-icon.png");
+        }
+    </style>
+    ';
+}
+
+// Hooks your functions into the correct filters
+function uasc_add_mce_button() {
+	// check user permissions
+	if ( !current_user_can( 'edit_posts' ) && !current_user_can( 'edit_pages' ) ) {
+		return;
+	}
+	// check if WYSIWYG is enabled
+	if ( 'true' == get_user_option( 'rich_editing' ) ) {
+		add_filter( 'mce_external_plugins', 'uasc_add_tinymce_plugin' );
+		add_filter( 'mce_buttons', 'uasc_register_mce_button' );
+	}
+}
+add_action('admin_head', 'uasc_add_mce_button');
+
+// Declare script for new button
+function uasc_add_tinymce_plugin( $plugin_array ) {
+	$plugin_array['uasc_mce_button'] = plugins_url('/js/uasc-mce-button.js', __FILE__);
+	return $plugin_array;
+}
+
+// Register new button in the editor
+function uasc_register_mce_button( $buttons ) {
+	array_push( $buttons, 'uasc_mce_button' );
+	return $buttons;
+}
+
+add_shortcode( 'UAS_guest', 'uasc_guest_sc' );
+
+function uasc_guest_sc( $atts, $content = null ) {
+    if ( is_user_logged_in() ) :
+        $content = '';
+        return $content;   
+    else :      
+        return $content;
+    endif;
+}
+
+add_shortcode( 'UAS_loggedin', 'uasc_loggedin_sc' );
+
+function uasc_loggedin_sc( $atts, $content = null ) {
+    if ( is_user_logged_in() ) :
+        return $content; 
+    else :      
+        $content = '';
+        return $content;
+    endif;
+}
+?>
